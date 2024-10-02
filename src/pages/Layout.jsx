5bf1e9a0b0ad983151ad8617f5f79043b5432ef6
Harey112 <apareceharey@gmail.com>
2024-10-02 21:43:29 +0800

    Add Few Global Styles

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+function Layout(){
+
+    return <>
+    <div className="base_container">
+        <div className="side_menu">
+
+        </div>
+        <div className="content">
+            
+        </div>
+    </div>
+    </>
+}
+
+export default Layout;
---
+++ src/pages/User/Login.jsx
@@ -0,0 +1,12 @@
+function Login(){
+    return<>
+    <div className="base_container center_child">
+        <div className="login_container center_child">
+            <p>jsdjs</p>
+        </div>
+    </div>
+    </>
+}
+
+
+export default Login;
