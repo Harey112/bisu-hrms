74dcf800eaa6687039c5655abe8a9e95eebc900a
Harey112 <apareceharey@gmail.com>
2024-10-20 13:56:49 +0000

    Setup firebase

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,11 +1,13 @@
 import './style.css';
+import base64Icons from '../../assets/icons';
 import Textfield from "../../fragments/input/textfield";
+
 
 function Login(){
     return<>
     <div className="base_container center_child" id="login_container">
         <div className="center_child" id="login_dialog">
-            <div id="login_main_logo"></div> 
+            <img id="login_main_logo" src={base64Icons.bisu} alt="" srcset="" />
             <div id="input_holder">
                 <Textfield type="email" name="Email" varName="email" id="login_email_input" value={null} placeholder={null}/>
                 <Textfield type="password" name="Password" id="login_email_input" value={null}/>
